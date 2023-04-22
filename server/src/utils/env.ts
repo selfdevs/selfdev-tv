81e5c87df7587f584eb2cf6c898c2678d3569258
cchampou <clement@champouillon.com>
2023-04-22 07:15:34 +0200

    fix: use environment variables

---
+++ server/src/utils/env.ts
@@ -0,0 +1,5 @@
+export const getEnvOrThrow = (key: string): string => {
+  const value = process.env[key];
+  if (!value) throw new Error(`Missing environment variable: ${key}`);
+  return value;
+};
